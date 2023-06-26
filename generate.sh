2c69bc7382d17cf66b134ef999b1ad057ee2bb31
Khaled Allam <kaaoaa505@gmail.com>
2023-06-26 14:30:16 +0300

    Auto create build folder Build grpc with the new grpc_js version

--- generate.sh
+++ generate.sh
@@ -1,4 +1,6 @@
+mkdir build;
+
 protoc -I=. ./proto/*.proto \
 --js_out=import_style=commonjs,binary:./build \
---grpc_out=./build \
+--grpc_out=grpc_js:./build \
 --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin`
